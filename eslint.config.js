e94cc6da464335467181bdff8440cb0f43aa223f
dragon3h <dragon3h@gmx.com>
2024-06-19 17:37:29 -0400

    build: added rules to Eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import { fixupConfigRules } from '@eslint/compat';
+import pluginReactAll from 'eslint-plugin-react/configs/all.js';
 
 export default [
-  {files: ["./src/**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  { files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
+  ...fixupConfigRules(pluginReactAll),
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    },
+  },
 ];
