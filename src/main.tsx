91c96851e167fa71f248ececf2f6b2b11bb1d5f5
dragon3h <dragon3h@gmx.com>
2024-06-22 15:55:23 -0400

    build: dockerized the app

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { I18nextProvider } from 'react-i18next';
 import App from './App.tsx';
 import i18n from './assets/i18n.ts';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
+    <BrowserRouter basename="/webclient">
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
