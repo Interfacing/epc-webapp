42bd059bb2c70347ea8bc4773ad0a14a7469abfe
dragon3h <dragon3h@gmx.com>
2024-06-25 15:40:50 -0400

    build: started production dockerizing

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/webclient/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   server: {
     port: 7777,
